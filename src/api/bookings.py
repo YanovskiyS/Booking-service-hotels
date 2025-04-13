ddd7fe3e499cb8ad68e1f142a6db6f40b5d397a2
Kholodetskiy <sergh.yanovskiy@inbox.ru>
2025-04-13 19:45:23 +0500

    add get endpoints

--- src/api/bookings.py
+++ src/api/bookings.py
@@ -1,6 +1,8 @@
 from fastapi import Query, Body, APIRouter
 from datetime import date
 
+from watchfiles import awatch
+
 from src.api.dependencies import UserIdDep
 from src.database import async_session_maker
 
@@ -16,12 +18,17 @@ router = APIRouter(prefix="/bookings", tags=["Бронирования"])
 async def create_booking(db: DBDep, user_id: UserIdDep,  booking_data: BookingAddRequest):
     room = await db.rooms.get_one_or_none(id=booking_data.room_id)
     total_price = (booking_data.date_to - booking_data.date_from).days * room.price
-    user = user_id
-    _booking_data = BookingAdd(user_id=user, price=total_price, **booking_data.model_dump())
-    async with async_session_maker() as session:
-        booking = await BookingsRepository(session).add(_booking_data)
-        await session.commit()
+    _booking_data = BookingAdd(user_id=user_id, price=total_price, **booking_data.model_dump())
+    booking = await db.bookings.add(_booking_data)
+    await db.commit()
 
     return {"status": "Ok", "data": booking}
 
+@router.get("")
+async def get_bookings(db: DBDep):
+    return await db.bookings.get_all()
 
+
+@router.get("/me")
+async def get_my_bookings(db: DBDep, user_id: UserIdDep):
+    return await db.bookings.get_filtered(user_id=user_id)
