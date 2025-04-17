9124d33d853c30ea2e18a9f195aa15bb987ef15d
Kholodetskiy <sergh.yanovskiy@inbox.ru>
2025-04-17 22:47:26 +0500

    update get room method

--- src/repositories/rooms.py
+++ src/repositories/rooms.py
@@ -1,20 +1,46 @@
-from pydantic import BaseModel
-from sqlalchemy import update, delete
+from datetime import date
+
+
+from sqlalchemy import select
+from sqlalchemy.orm import selectinload, joinedload
 
 from src.models.rooms import RoomsOrm
 from src.repositories.base import BaseRepository
-from src.schemas.rooms import Room
+from src.repositories.utils import rooms_ids_for_booking
+from src.schemas.rooms import Room, RoomWithRels
 
 
 class RoomsRepository(BaseRepository):
     model = RoomsOrm
     schema = Room
 
-    async def edit(self, data: BaseModel, exclude_unset: bool = False, **filter_by):
-        product_update = (update(self.model).filter_by(**filter_by)
-                          .values(data.model_dump(exclude_unset=exclude_unset)))
-        await self.session.execute(product_update)
 
-    '''async def delete(self, **filter_by):
-        delete_stmt = delete(self.model).filter_by(**filter_by)
-        await self.session.execute(delete_stmt)'''
+    async def get_filtered_by_time(self, hotel_id,
+                                   date_from: date,
+                                   date_to: date
+                                   ):
+        rooms_ids_to_get = rooms_ids_for_booking(date_from, date_to, hotel_id)
+
+        query = (
+            select(self.model)
+            .options(selectinload(self.model.facilities))
+            .filter(RoomsOrm.id.in_(rooms_ids_to_get))
+        )
+        result = await self.session.execute(query)
+        return [RoomWithRels.model_validate(model, from_attributes=True) for model in result.unique().scalars().all()]
+
+    async def get_one_room_with_facilities(self, hotel_id: int, room_id: int):
+        query = (
+            select(self.model)
+            .options(selectinload(self.model.facilities))
+            .filter_by(hotel_id=hotel_id, id=room_id)
+        )
+        result = await self.session.execute(query)
+        return  result.unique().scalars().one_or_none()
+
+
+
+
+
+
+
