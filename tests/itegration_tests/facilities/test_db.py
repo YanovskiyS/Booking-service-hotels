f923347e1a3cf136022a20c05bd2a925346020ce
Kholodetskiy <sergh.yanovskiy@inbox.ru>
2025-04-28 19:50:52 +0500

    add facilities tests

--- tests/itegration_tests/facilities/test_db.py
+++ tests/itegration_tests/facilities/test_db.py
@@ -2,7 +2,7 @@ from src.schemas.facilities import FacilityAdd
 
 
 async def test_add_facility(db):
-    facility_data = FacilityAdd(title="WIFI")
+    facility_add_data = FacilityAdd(title="WIFI")
 
-    await db.facilities.add(facility_data)
+    await db.facilities.add(facility_add_data)
     await db.commit()
